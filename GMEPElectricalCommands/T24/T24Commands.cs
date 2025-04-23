169afef626fd707b16c8e065329827deb017b1e2
Ethan Stafford <ethanstafford.dev@gmail.com>
2025-04-23 13:08:24 -0700

    senium

---
+++ GMEPElectricalCommands/T24/T24Commands.cs
@@ -0,0 +1,15 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricalCommands.T24 {
+  class T24Commands {
+    [CommandMethod("LTI")]
+    public void LTI() {
+
+    }
+  }
+}
