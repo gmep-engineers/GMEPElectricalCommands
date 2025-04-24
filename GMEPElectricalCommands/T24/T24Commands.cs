bae17f1802a0ecb4eb42034c9ac2b2d82c2d9241
Ethan Stafford <ethanstafford.dev@gmail.com>
2025-04-24 14:04:05 -0700

    meow

--- GMEPElectricalCommands/T24/T24Commands.cs
+++ GMEPElectricalCommands/T24/T24Commands.cs
@@ -1,15 +1,33 @@
 ﻿using Autodesk.AutoCAD.Runtime;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.ApplicationServices;
+using DocumentFormat.OpenXml.Drawing.Diagrams;
+using Emgu.CV.Ocl;
+using OpenQA.Selenium.Support.UI;
 namespace ElectricalCommands.T24 {
-  class T24Commands {
+  public class T24Commands {
     [CommandMethod("LTI")]
     public void LTI() {
+      Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+      string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+      string projectFolder = Path.GetDirectoryName(assemblyLocation);
+      string ChromePath = Path.Combine(projectFolder, "selenium-manager", "windows", "chromedriver.exe");
 
+
+
+      using (IWebDriver driver = new ChromeDriver(ChromePath) { }) {
+       
+      }
     }
   }
 }
